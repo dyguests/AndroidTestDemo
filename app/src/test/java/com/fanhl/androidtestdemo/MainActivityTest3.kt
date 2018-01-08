5853f059d2dfaef17e3d374db40a9cc312cf9470
fanhl <fanhonglin@klicen.com>
2018-01-08 11:33:36 +0800

    annotation

--- app/src/test/java/com/fanhl/androidtestdemo/MainActivityTest3.kt
+++ app/src/test/java/com/fanhl/androidtestdemo/MainActivityTest3.kt
@@ -1,6 +1,7 @@
 package com.fanhl.androidtestdemo
 
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
 
 /**
@@ -10,18 +11,21 @@ import org.junit.Test
  * @author fanhl
  */
 class MainActivityTest3 {
+    private lateinit var testClass: MainActivity
+
+    @Before
+    fun setup() {
+        testClass = MainActivity()
+    }
+
     @Test
     fun priceFormat_test() {
-        val testClass = MainActivity()
-
         Assert.assertEquals("1", testClass.priceFormat("1"))
         Assert.assertEquals("1.1", testClass.priceFormat("1.14"))
     }
 
     @Test
     fun getX_test() {
-        val testClass = MainActivity()
-
         Assert.assertEquals(1, testClass.getX())
     }
 }
