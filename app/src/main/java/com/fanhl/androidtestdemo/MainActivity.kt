f27dbd374175fd4cae4207b6b060eb4168044d8d
fanhl <fanhonglin@klicen.com>
2018-01-08 10:58:42 +0800

    Init Commit.

--- app/src/main/java/com/fanhl/androidtestdemo/MainActivity.kt
+++ app/src/main/java/com/fanhl/androidtestdemo/MainActivity.kt
@@ -3,10 +3,8 @@ package com.fanhl.androidtestdemo
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,19 +19,17 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+    /**
+     * 金额最多保留一位小数
+     */
+    fun priceFormat(price: String?): String? {
+        if (price == null) {
+            return null
+        }
+        return try {
+            DecimalFormat("#.#").format(price.toFloat())
+        } catch (e: Exception) {
+            price
         }
     }
 }
