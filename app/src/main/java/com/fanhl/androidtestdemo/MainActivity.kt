9fdc265133fb5f0c13783bc5e5c000b7106f0fc8
fanhl <fanhonglin@klicen.com>
2018-01-08 11:26:02 +0800

    test

--- app/src/main/java/com/fanhl/androidtestdemo/MainActivity.kt
+++ app/src/main/java/com/fanhl/androidtestdemo/MainActivity.kt
@@ -32,4 +32,9 @@ class MainActivity : AppCompatActivity() {
             price
         }
     }
+
+    /**
+     * 返回1
+     */
+    fun getX() = 1
 }
---
+++ app/src/test/java/com/fanhl/androidtestdemo/MainActivityTest3.kt
@@ -0,0 +1,27 @@
+package com.fanhl.androidtestdemo
+
+import org.junit.Assert
+import org.junit.Test
+
+/**
+ * desc:
+ * date: 2018/1/8
+ *
+ * @author fanhl
+ */
+class MainActivityTest3 {
+    @Test
+    fun priceFormat_test() {
+        val testClass = MainActivity()
+
+        Assert.assertEquals("1", testClass.priceFormat("1"))
+        Assert.assertEquals("1.1", testClass.priceFormat("1.14"))
+    }
+
+    @Test
+    fun getX_test() {
+        val testClass = MainActivity()
+
+        Assert.assertEquals(1, testClass.getX())
+    }
+}
